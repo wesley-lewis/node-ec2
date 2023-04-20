b23fb8aa1410c33f65c856522346b4513d41558b
wesley-lewis <lewis.wes.23@gmail.com>
2023-04-20 09:44:55 +0530

    Data files added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express"
+import data from "./data.json" assert { type: 'json'}
 
 const app = express()
 
 app.listen(5001, () => console.log('API running on port 5001'))
 
-app.get('/', (req, res) => res.json('API is running'))
+app.get('/', (req, res) => res.send(data))
